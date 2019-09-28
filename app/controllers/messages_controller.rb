d193d9d9f49dce64f3f045c95ecffdeb54c8559d
akira-kotaka <taakkierru@icloud.com>
2019-09-28 15:30:48 +0900

    edit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,13 @@
 class MessagesController < ApplicationController
   before_action :set_group
 
+  if @message.save
+    redirect_to do |format|
+      format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' }
+      format.json
+    end
+  end
+
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
